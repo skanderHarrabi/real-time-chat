b13d9c441659158e7497101ffcddfaa5687b4c07
skander <skanderharrabi75@gmail.com>
2020-08-03 04:37:03 +0100

    Updates

---
+++ precache-manifest.0c3ab6f2174aa9aee6b208dfe92485dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e72a760f0e9476e93746a24b050b170",
+    "url": "/real-time-chat/index.html"
+  },
+  {
+    "revision": "ffb66a787e3a2d1a3784",
+    "url": "/real-time-chat/static/css/main.e89a8b81.chunk.css"
+  },
+  {
+    "revision": "164da5d9caf1cf623c7a",
+    "url": "/real-time-chat/static/js/2.81c3b830.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/real-time-chat/static/js/2.81c3b830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffb66a787e3a2d1a3784",
+    "url": "/real-time-chat/static/js/main.4d63bb2d.chunk.js"
+  },
+  {
+    "revision": "7692d75572cb693897c4",
+    "url": "/real-time-chat/static/js/runtime-main.9193341c.js"
+  },
+  {
+    "revision": "d65552c0003bca4c65805a13f4b86b31",
+    "url": "/real-time-chat/static/media/laptop.d65552c0.png"
+  }
+]);
